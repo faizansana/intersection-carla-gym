564e23fa86ca3ef097416fc52ee01363b185a022
Faizan Sana <faizansana01@gmail.com>
2023-05-19 00:33:59 +0000

    Update env tester

--- test_env.py
+++ test_env.py
@@ -1,7 +1,3 @@
-
-import sys
-sys.path.append("/home/docker/src/src/carla-0.9.10-py3.7-linux-x86_64.egg")
-
 import stable_baselines3.common.env_checker
 import yaml
 
@@ -9,6 +5,6 @@ import carla_env_custom
 
 if __name__ == "__main__":
     cfg = yaml.safe_load(open("config.yaml", "r"))
-    env = carla_env_custom.CarlaEnv(cfg=cfg)
+    env = carla_env_custom.CarlaEnv(cfg=cfg, host="intersection-driving-carla_server_high-1")
 
-    stable_baselines3.common.env_checker.check_env(env, warn=False)
+    stable_baselines3.common.env_checker.check_env(env, warn=True)
