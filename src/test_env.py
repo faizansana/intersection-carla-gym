94397ebfe12fdf425dbbc5771fdc35c365ef9cb4
Faizan Sana <64595558+faizansana@users.noreply.github.com>
2024-02-14 19:32:08 -0500

    Send info over UDP to PlotJuggler to plot control, acceleration and velocity curves (#25)
    * Add UDP server class
    * Incorporate UDP server into CARLA env
    * Update udp server with control
    * Add udp server to logger
    * Add ego vehicle dynamics to udp server
    * Fix function signature in test_env.py
    * Update IMU more verbose

--- src/test_env.py
+++ src/test_env.py
@@ -5,6 +5,6 @@ import carla_env_custom
 
 if __name__ == "__main__":
     cfg = yaml.safe_load(open("config_discrete.yaml", "r"))
-    env = carla_env_custom.CarlaEnv(cfg=cfg, host="carla_server", port=2000, tm_port=9002)
+    env = carla_env_custom.CarlaEnv(cfg=cfg, host="carla_server", port=2000, udp_server=True)
 
     stable_baselines3.common.env_checker.check_env(env, warn=True)
